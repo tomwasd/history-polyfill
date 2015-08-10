a7c0c9866eb0af274977f6903ba52167d5de70c0
tomwasd <tom@wasd.co.uk>
2015-08-10 12:08:41 +0100

    First commit

---
+++ settings.js
@@ -0,0 +1,6 @@
+console.log('using tomwasd:history-polyfill');
+
+// Make sure that polyfilled links are redirected to correct links in 
+// supporting browsers. Enables sharing links between IE and non-IE
+// e.g. http://example.com/#/some-path -> http://example.com/some-path
+history.redirect();
